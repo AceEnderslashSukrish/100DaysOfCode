6a068fd29df6cb7223b69708072aa0b1245c9804
Sukrish Gokulakrishnan <enderslash@outlook.com>
2022-04-27 23:47:12 -0400

    Added common operations
    Addition, Subtraction, Multiplication, Division

---
+++ Calculator/Operations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Operations
+    {
+        float _num1 { get; set; }
+        float _num2 { get; set; }
+
+        public Operations(float num1, float num2)
+        {
+            _num1 = num1;
+            _num2 = num2;
+        }
+
+        public float Add()
+        {
+            return _num1 + _num2;
+        }
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Subtract()
+        {
+            return (_num1 - _num2);
+        }
+
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Divide()
+        {
+            return _num1 / _num2;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+        public float Multiply()
+        {
+            return _num1 * _num2;
+        }
+
+        public float Multiple(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Calculator Application");
+Console.WriteLine();
+Console.WriteLine("What would you like to do today?");
+Console.WriteLine("1.Add\n2.Subtract\n3.Multiply\n4.Divide");
+int chc = Convert.ToInt32(Console.ReadLine());
