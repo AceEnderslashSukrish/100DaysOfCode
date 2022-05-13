59d9ce1ae1a2394e89f1cb9b575f3bc6d92ad45c
Sukrish Gokulakrishnan <enderslash@outlook.com>
2022-05-13 15:09:21 -0400

    Created new WinForms App
    Transforming calculator into WinForms
    Created First Row - No functionality yet.

---
+++ CalculatorWinForms/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CalculatorWinForms
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValue = new System.Windows.Forms.Label();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValue
+            // 
+            this.lblValue.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblValue.AutoEllipsis = true;
+            this.lblValue.BackColor = System.Drawing.Color.Transparent;
+            this.lblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblValue.Location = new System.Drawing.Point(12, 9);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblValue.Size = new System.Drawing.Size(401, 136);
+            this.lblValue.TabIndex = 0;
+            this.lblValue.Text = "0";
+            this.lblValue.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnDiv.FlatAppearance.BorderSize = 0;
+            this.btnDiv.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btnDiv.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnDiv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnDiv.Location = new System.Drawing.Point(12, 165);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(96, 60);
+            this.btnDiv.TabIndex = 1;
+            this.btnDiv.Text = "%";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnClearAll.FlatAppearance.BorderSize = 0;
+            this.btnClearAll.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btnClearAll.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnClearAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearAll.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnClearAll.Location = new System.Drawing.Point(114, 165);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(96, 60);
+            this.btnClearAll.TabIndex = 2;
+            this.btnClearAll.Text = "CE";
+            this.btnClearAll.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnClear.Location = new System.Drawing.Point(216, 165);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(96, 60);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btnBack.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnBack.Location = new System.Drawing.Point(317, 165);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(96, 60);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "<==";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // CalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(425, 569);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.lblValue);
+            this.MinimumSize = new System.Drawing.Size(443, 616);
+            this.Name = "CalculatorForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.CalculatorForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
